61908a09a25cad22be11448da0de655a65b8d090
A7xSV <sabhya.verma16@gmail.com>
2018-03-05 02:04:24 +0530

    Add changed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import reddit from './reddit';
+
 const searchInput = document.getElementById('search-input');
 const searchForm  = document.getElementById('search-form');
 
@@ -21,7 +23,33 @@ searchForm.addEventListener('submit', e => {
   searchInput.value = '';
 
   // Search Reddit
-  reddit.search(searchTerm, searchLimit, sortBy);
+  reddit.search(searchTerm, searchLimit, sortBy)
+    .then(results => {
+      let output = '<div class="card-columns">';
+      
+      // Loop through the results and add card
+      results.forEach(post => {
+        // Check if the post has an image
+        const image = post.preview ? post.preview.images[0].source.url : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg';
+
+        output += `
+          <div class="card">
+            <img class="card-img-top" src="${image}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">${post.title}</h5>
+              <p class="card-text">${truncateText(post.selftext, 100)}</p>
+              <a href="${post.url}" class="btn btn-primary" target="_blank">Read More</a>
+              <hr>
+              <span class="badge badge-secondary">Subreddit: ${post.subreddit}</span>
+              <span class="badge badge-dark">Score: ${post.score}</span>
+            </div>
+          </div>
+        `
+      })
+      
+      output += '</div>';
+      document.getElementById('results').innerHTML = output;
+    });
 
   // Prevent the form from actually submitting
   e.preventDefault();
@@ -53,4 +81,19 @@ function showMessage(message, className) {
 
   // Alert timeout
   setTimeout(() => document.querySelector('.alert').remove(), 3000);
+}
+
+/**
+ * Shorten a piece of text.
+ * @param {string} text - The text string to shorten.
+ * @param {string} limit - The limit to shorten upto.
+ */
+function truncateText(text, limit) {
+  // Make sure it's the last word
+  const shortened = text.indexOf(' ', limit);
+
+  if (shortened == -1) {
+    return text;
+  }
+  return text.substring(0, shortened);
 }
